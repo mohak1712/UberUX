e6b4432a2303c41d8ea0e2e51269812b330e92b1
Mohak <mohakpuri1712@gmail.com>
2017-07-28 00:51:40 +0530

    Added LoginWithPhone activity and layout

---
+++ app/src/main/java/mohak/uberux/LoginWithPhone.java
@@ -0,0 +1,161 @@
+package mohak.uberux;
+
+import android.content.Context;
+import android.support.v4.util.LogWriter;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.transition.ChangeBounds;
+import android.transition.Transition;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.view.animation.DecelerateInterpolator;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+
+import com.github.jorgecastilloprz.FABProgressCircle;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class LoginWithPhone extends AppCompatActivity {
+
+    @BindView(R.id.ivback)
+    ImageView ivBack;
+
+    @BindView(R.id.etPhoneNo)
+    EditText etPhoneNo;
+
+    @BindView(R.id.tvMoving)
+    TextView tvMoving;
+
+    @BindView(R.id.ivFlag)
+    ImageView ivFlag;
+
+    @BindView(R.id.tvCode)
+    TextView tvCode;
+
+    @BindView(R.id.fabProgressCircle)
+    FABProgressCircle fabProgressCircle;
+
+    @BindView(R.id.rootFrame)
+    FrameLayout rootFrame;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_with_phone);
+        ButterKnife.bind(this);
+        setupWindowAnimations();
+
+    }
+
+    private void setupWindowAnimations() {
+
+        ChangeBounds enterTransition = new ChangeBounds();
+        enterTransition.setDuration(1000);
+        enterTransition.setInterpolator(new DecelerateInterpolator(4));
+        enterTransition.addListener(enterTransitionListener);
+        getWindow().setSharedElementEnterTransition(enterTransition);
+
+        ChangeBounds returnTransition = new ChangeBounds();
+        returnTransition.setDuration(1000);
+        returnTransition.addListener(returnTransitionListener);
+        getWindow().setSharedElementReturnTransition(returnTransition);
+
+    }
+
+    Transition.TransitionListener enterTransitionListener = new Transition.TransitionListener() {
+        @Override
+        public void onTransitionStart(Transition transition) {
+            ivBack.setImageAlpha(0);
+        }
+
+        @Override
+        public void onTransitionEnd(Transition transition) {
+
+            ivBack.setImageAlpha(255);
+            Animation animation = AnimationUtils.loadAnimation(LoginWithPhone.this,R.anim.slide_right);
+            ivBack.startAnimation(animation);
+
+            InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+            imm.toggleSoftInput(InputMethodManager.SHOW_FORCED, 0);
+
+        }
+
+        @Override
+        public void onTransitionCancel(Transition transition) {
+
+        }
+
+        @Override
+        public void onTransitionPause(Transition transition) {
+
+        }
+
+        @Override
+        public void onTransitionResume(Transition transition) {
+
+        }
+    };
+
+
+    Transition.TransitionListener returnTransitionListener = new Transition.TransitionListener() {
+        @Override
+        public void onTransitionStart(Transition transition) {
+
+            InputMethodManager imm = (InputMethodManager) getSystemService(
+                    Context.INPUT_METHOD_SERVICE);
+            imm.hideSoftInputFromWindow(etPhoneNo.getWindowToken(), 0);
+            tvMoving.setText(null);
+            tvMoving.setHint(getString(R.string.enter_no));
+            ivFlag.setImageAlpha(0);
+            tvCode.setAlpha(0);
+            etPhoneNo.setVisibility(View.GONE);
+            etPhoneNo.setCursorVisible(false);
+            etPhoneNo.setBackground(null);
+            Animation animation = AnimationUtils.loadAnimation(LoginWithPhone.this,R.anim.slide_left);
+            ivBack.startAnimation(animation);
+        }
+
+        @Override
+        public void onTransitionEnd(Transition transition) {
+
+
+        }
+
+        @Override
+        public void onTransitionCancel(Transition transition) {
+
+        }
+
+        @Override
+        public void onTransitionPause(Transition transition) {
+
+        }
+
+        @Override
+        public void onTransitionResume(Transition transition) {
+
+        }
+    };
+
+
+    @OnClick(R.id.fabProgressCircle)
+    void nextPager(){
+
+        etPhoneNo.setCursorVisible(false);
+        rootFrame.setAlpha(0.4f);
+        fabProgressCircle.show();
+    }
+
+    @OnClick(R.id.ivback)
+    void startReturnTransition() {
+        super.onBackPressed();
+    }
+}
