eb944300d89f111eb2cebd66e7f0b400ef059a79
Mohak <mohakpuri1712@gmail.com>
2017-07-29 14:59:53 +0530

    added password activity

---
+++ app/src/main/java/mohak/uberux/PasswordActivity.java
@@ -0,0 +1,93 @@
+package mohak.uberux;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v4.app.ActivityOptionsCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.transition.Slide;
+import android.transition.Transition;
+import android.transition.TransitionInflater;
+import android.view.Gravity;
+import android.view.animation.AccelerateDecelerateInterpolator;
+import android.view.animation.AccelerateInterpolator;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.view.animation.DecelerateInterpolator;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
+import android.widget.FrameLayout;
+import android.widget.Toast;
+
+import com.github.jorgecastilloprz.FABProgressCircle;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+import static android.view.Gravity.LEFT;
+import static android.view.Gravity.RIGHT;
+
+
+public class PasswordActivity extends AppCompatActivity {
+
+
+    @BindView(R.id.fabProgressCircle)
+    FABProgressCircle fabProgressCircle;
+
+    @BindView(R.id.rootFrame)
+    FrameLayout rootFrame;
+
+    @BindView(R.id.etPass)
+    EditText etPass;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_password);
+
+        ButterKnife.bind(this);
+
+        Slide enterSlide = new Slide(RIGHT);
+        enterSlide.setDuration(700);
+        enterSlide.addTarget(R.id.llphone);
+        enterSlide.setInterpolator(new DecelerateInterpolator(2));
+        getWindow().setEnterTransition(enterSlide);
+
+        Slide returnSlide = new Slide(RIGHT);
+        returnSlide.setDuration(700);
+        returnSlide.addTarget(R.id.llphone);
+        returnSlide.setInterpolator(new DecelerateInterpolator());
+        getWindow().setReturnTransition(returnSlide);
+
+
+    }
+
+
+    @OnClick(R.id.fabProgressCircle)
+    void nextActivity() {
+        etPass.setCursorVisible(false);
+        rootFrame.setAlpha(0.4f);
+        fabProgressCircle.show();
+
+        InputMethodManager imm = (InputMethodManager) getSystemService(
+                Context.INPUT_METHOD_SERVICE);
+        imm.hideSoftInputFromWindow(etPass.getWindowToken(), 0);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                Intent intent = new Intent(PasswordActivity.this, MapActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 1000);
+    }
+
+    @OnClick(R.id.ivback)
+    void back() {
+        onBackPressed();
+    }
+}
