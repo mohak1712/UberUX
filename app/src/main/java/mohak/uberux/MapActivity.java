d3c88f1d7661993dd936fd03bf32a6980c9d015e
Mohak <mohakpuri1712@gmail.com>
2017-08-01 13:51:21 +0530

    viewpager animation added

--- app/src/main/java/mohak/uberux/MapActivity.java
+++ app/src/main/java/mohak/uberux/MapActivity.java
@@ -2,21 +2,36 @@ package mohak.uberux;
 
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
+import android.animation.ArgbEvaluator;
+import android.animation.ValueAnimator;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
+import android.support.v4.view.ViewPager;
 import android.os.Bundle;
+import android.transition.TransitionManager;
+import android.util.DisplayMetrics;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewAnimationUtils;
-import android.view.animation.DecelerateInterpolator;
+import android.view.ViewGroup;
+import android.view.animation.AccelerateInterpolator;
 import android.widget.FrameLayout;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
+
+import java.util.Arrays;
+import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
-public class MapActivity extends AppCompatActivity {
+public class MapActivity extends BaseActivity {
 
     @BindView(R.id.rootFrame)
     FrameLayout rootFrame;
@@ -24,32 +39,141 @@ public class MapActivity extends AppCompatActivity {
     @BindView(R.id.rootll)
     LinearLayout rootll;
 
+    @BindView(R.id.viewPager)
+    ViewPager viewPager;
+
+    @BindView(R.id.rlwhere)
+    RelativeLayout rlWhere;
+
+    @BindView(R.id.ivHome)
+    ImageView ivHome;
+
+    @BindView(R.id.tvWhereTo)
+    TextView tvWhereto;
+
+    ArgbEvaluator argbEvaluator;
+
+    private SupportMapFragment mapFragment;
+    private int devHeight, devWidth;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
         ButterKnife.bind(this);
 
-        rootFrame.post(new Runnable() {
-            @Override
-            public void run() {
+        mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(MapActivity.this);
 
-                startRevealAnimation();
-            }
-        });
+        argbEvaluator = new ArgbEvaluator();
+
+        DisplayMetrics displayMetrics = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+        devHeight = displayMetrics.heightPixels;
+        devWidth = displayMetrics.widthPixels;
+
+        setUpPagerAdapter();
+        viewPager.setClipToPadding(false);
+        viewPager.setPageMargin(-devWidth / 2);
+
+        viewPager.addOnPageChangeListener(pageChangeListener);
+        viewPager.setPageTransformer(true, pageTransformer);
+
+
     }
+
+    ViewPager.PageTransformer pageTransformer = new ViewPager.PageTransformer() {
+        @Override
+        public void transformPage(View page, float position) {
+
+
+            if (position < -1) { // [-Infinity,-1)
+
+
+            } else if (position <= 1) { // [-1,1]
+
+                if (position >= -1 && position < 0) {
+
+                    LinearLayout uberEco = (LinearLayout) page.findViewById(R.id.lluberEconomy);
+                    TextView uberEcoTv = (TextView) page.findViewById(R.id.tvuberEconomy);
+
+                    if (uberEco != null && uberEcoTv != null) {
+
+                        uberEcoTv.setTextColor((Integer) argbEvaluator.evaluate(-2 * position, getResources().getColor(R.color.black)
+                                , getResources().getColor(R.color.grey)));
+
+                        uberEcoTv.setTextSize(16 + 4 * position);
+                        uberEco.setX((page.getWidth() * position));
+
+                    }
+
+                } else if (position >= 0 && position <= 1) {
+
+                    TextView uberPreTv = (TextView) page.findViewById(R.id.tvuberPre);
+                    LinearLayout uberPre = (LinearLayout) page.findViewById(R.id.llUberPre);
+
+                    if (uberPreTv != null && uberPre != null) {
+
+                        uberPreTv.setTextColor((Integer) new ArgbEvaluator().evaluate((1 - position), getResources().getColor(R.color.grey)
+                                , getResources().getColor(R.color.black)));
+
+                        uberPreTv.setTextSize(12 + 4 * (1 - position));
+                        uberPre.setX(uberPre.getLeft() + (page.getWidth() * (position)));
+
+
+
+                    }
+
+
+                }
+
+            }
+        }
+    };
+
+
+    ViewPager.OnPageChangeListener pageChangeListener = new ViewPager.OnPageChangeListener() {
+        @Override
+        public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+
+        }
+
+        @Override
+        public void onPageSelected(int position) {
+
+        }
+
+        @Override
+        public void onPageScrollStateChanged(int state) {
+
+        }
+    };
+
+    @OnClick(R.id.ivHome)
+    void showViewPagerWithTransition() {
+
+        TransitionManager.beginDelayedTransition(rootFrame);
+        viewPager.setVisibility(View.VISIBLE);
+        ivHome.setVisibility(View.INVISIBLE);
+        rlWhere.setVisibility(View.INVISIBLE);
+
+        mMap.setPadding(0, 0, 0, viewPager.getHeight());
+
+    }
+
 
     void startRevealAnimation() {
 
         int cx = rootFrame.getMeasuredWidth() / 2;
         int cy = rootFrame.getMeasuredHeight() / 2;
 
-
         Animator anim =
                 ViewAnimationUtils.createCircularReveal(rootll, cx, cy, 50, rootFrame.getWidth());
 
-        anim.setDuration(1000);
-        anim.setInterpolator(new DecelerateInterpolator());
+        anim.setDuration(500);
+        anim.setInterpolator(new AccelerateInterpolator(2));
         anim.addListener(new AnimatorListenerAdapter() {
 
             @Override
@@ -61,15 +185,47 @@ public class MapActivity extends AppCompatActivity {
             public void onAnimationEnd(Animator animation) {
                 super.onAnimationEnd(animation);
 
+                rlWhere.setVisibility(View.VISIBLE);
+                ivHome.setVisibility(View.VISIBLE);
             }
         });
 
         anim.start();
     }
 
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        super.onMapReady(googleMap);
+        startRevealAnimation();
+
+
+    }
+
+    private void setUpPagerAdapter() {
+
+        List<Integer> data = Arrays.asList(0, 1);
+        CarsPagerAdapter adapter = new CarsPagerAdapter(data);
+        viewPager.setAdapter(adapter);
+    }
+
     @Override
     public void onBackPressed() {
+
+        if (viewPager.getVisibility() == View.VISIBLE) {
+
+            TransitionManager.beginDelayedTransition(rootFrame);
+            viewPager.setVisibility(View.INVISIBLE);
+            mMap.setPadding(0, 0, 0, 0);
+            ivHome.setVisibility(View.VISIBLE);
+            rlWhere.setVisibility(View.VISIBLE);
+
+            return;
+        }
+
         startActivity(new Intent(this, LoginActivity.class));
         finish();
     }
+
+
 }
