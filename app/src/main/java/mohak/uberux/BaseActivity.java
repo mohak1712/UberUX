0d1231953bc73631ade6c6903d31a56d39a089db
Mohak <mohakpuri1712@gmail.com>
2017-08-02 11:53:30 +0530

    overlay animation added

--- app/src/main/java/mohak/uberux/BaseActivity.java
+++ app/src/main/java/mohak/uberux/BaseActivity.java
@@ -1,30 +1,58 @@
 package mohak.uberux;
 
 import android.Manifest;
+import android.animation.AnimatorSet;
+import android.animation.ValueAnimator;
+import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.location.Location;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.animation.LinearInterpolator;
 import android.widget.Toast;
 
+import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
+import com.google.android.gms.common.GooglePlayServicesRepairableException;
+import com.google.android.gms.common.api.Status;
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
-import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.ui.PlaceAutocomplete;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.GroundOverlay;
+import com.google.android.gms.maps.model.GroundOverlayOptions;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MapStyleOptions;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.PolylineOptions;
 import com.google.android.gms.tasks.OnSuccessListener;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 public abstract class BaseActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     GoogleMap mMap;
     Location userLocation;
     private FusedLocationProviderClient mFusedLocationClient;
+    public static final int PLACE_AUTOCOMPLETE_REQUEST_CODE = 100;
+    private LatLng destination;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,11 +69,13 @@ public abstract class BaseActivity extends AppCompatActivity implements OnMapRea
      * it inside the SupportMapFragment. This method will only be triggered once the user has
      * installed Google Play services and returned to the app.
      */
-
     @Override
     public void onMapReady(GoogleMap googleMap) {
 
         mMap = googleMap;
+        mMap.getUiSettings().setMyLocationButtonEnabled(false);
+        mMap.setMyLocationEnabled(true);
+        mMap.setMaxZoomPreference(20);
 
         MapStyleOptions style = MapStyleOptions.loadRawResourceStyle(
                 this, R.raw.maps_style);
@@ -63,14 +93,10 @@ public abstract class BaseActivity extends AppCompatActivity implements OnMapRea
 
                                 CameraPosition cameraPosition = new CameraPosition.Builder()
                                         .target(new LatLng(userLocation.getLatitude(), userLocation.getLongitude()))
-                                        .zoom(16)
+                                        .zoom(17)
                                         .build();
 
-//                                MarkerOptions markerOptions = new MarkerOptions()
-//                                        .position(new LatLng(userLocation.getLatitude(),userLocation.getLongitude()))
-//                                        .icon();
-
-//                                mMap.addMarker(markerOptions);
+                                addOverlay(new LatLng(userLocation.getLatitude(), userLocation.getLongitude()));
                                 mMap.moveCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
 
                             } else {
@@ -79,6 +105,19 @@ public abstract class BaseActivity extends AppCompatActivity implements OnMapRea
                         }
                     });
 
+    }
+
+
+    public void openPlaceAutoCompleteView() {
+
+        try {
+            Intent intent =
+                    new PlaceAutocomplete.IntentBuilder(PlaceAutocomplete.MODE_FULLSCREEN)
+                            .build(this);
+            startActivityForResult(intent, PLACE_AUTOCOMPLETE_REQUEST_CODE);
+        } catch (GooglePlayServicesRepairableException | GooglePlayServicesNotAvailableException e) {
+            // TODO: Handle the error.
+        }
 
     }
 
@@ -101,5 +140,112 @@ public abstract class BaseActivity extends AppCompatActivity implements OnMapRea
         return null;
     }
 
+
+    public LatLng getDestinationLatLong() {
+
+        if (destination != null)
+            return destination;
+        else
+            return null;
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == PLACE_AUTOCOMPLETE_REQUEST_CODE) {
+            if (resultCode == RESULT_OK) {
+                Place place = PlaceAutocomplete.getPlace(this, data);
+                destination = place.getLatLng();
+                addMarker(destination);
+
+            } else if (resultCode == PlaceAutocomplete.RESULT_ERROR) {
+                Status status = PlaceAutocomplete.getStatus(this, data);
+                Toast.makeText(this, "Error " + status, Toast.LENGTH_SHORT).show();
+
+            } else if (resultCode == RESULT_CANCELED) {
+                // The user canceled the operation.
+            }
+        }
+    }
+
+    private void addMarker(LatLng destination) {
+
+        MarkerOptions options = new MarkerOptions();
+        options.position(destination);
+        options.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_RED));
+        mMap.addMarker(options);
+
+        setUpPolyLine();
+    }
+
+    protected abstract void setUpPolyLine();
+
+
+    private Bitmap drawableToBitmap(Drawable drawable) {
+        Bitmap bitmap = null;
+
+        if (drawable instanceof BitmapDrawable) {
+            BitmapDrawable bitmapDrawable = (BitmapDrawable) drawable;
+            if (bitmapDrawable.getBitmap() != null) {
+                return bitmapDrawable.getBitmap();
+            }
+        }
+
+        if (drawable.getIntrinsicWidth() <= 0 || drawable.getIntrinsicHeight() <= 0) {
+            bitmap = Bitmap.createBitmap(1, 1, Bitmap.Config.ARGB_8888);
+        } else {
+            bitmap = Bitmap.createBitmap(drawable.getIntrinsicWidth(), drawable.getIntrinsicHeight(), Bitmap.Config.ARGB_8888);
+        }
+
+        Canvas canvas = new Canvas(bitmap);
+        drawable.setBounds(0, 0, canvas.getWidth(), canvas.getHeight());
+        drawable.draw(canvas);
+        return bitmap;
+    }
+
+    public void addOverlay(LatLng place) {
+
+        GroundOverlay groundOverlay = mMap.addGroundOverlay(new
+                GroundOverlayOptions()
+                .position(place, 100)
+                .transparency(0.5f)
+                .image(BitmapDescriptorFactory.fromBitmap(drawableToBitmap(getDrawable(R.drawable.map_overlay)))));
+
+        startOverlayAnimation(groundOverlay);
+    }
+
+
+    private void startOverlayAnimation(final GroundOverlay groundOverlay) {
+
+        AnimatorSet animatorSet = new AnimatorSet();
+
+        ValueAnimator vAnimator = ValueAnimator.ofInt(0, 100);
+        vAnimator.setRepeatCount(ValueAnimator.INFINITE);
+        vAnimator.setRepeatMode(ValueAnimator.RESTART);
+        vAnimator.setInterpolator(new LinearInterpolator());
+        vAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator valueAnimator) {
+                final Integer val = (Integer) valueAnimator.getAnimatedValue();
+                groundOverlay.setDimensions(val);
+
+            }
+        });
+
+        ValueAnimator tAnimator = ValueAnimator.ofFloat(0, 1);
+        tAnimator.setRepeatCount(ValueAnimator.INFINITE);
+        tAnimator.setRepeatMode(ValueAnimator.RESTART);
+        tAnimator.setInterpolator(new LinearInterpolator());
+        tAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator valueAnimator) {
+                Float val = (Float) valueAnimator.getAnimatedValue();
+                groundOverlay.setTransparency(val);
+            }
+        });
+
+        animatorSet.setDuration(3000);
+        animatorSet.playTogether(vAnimator, tAnimator);
+        animatorSet.start();
+    }
 
 }
